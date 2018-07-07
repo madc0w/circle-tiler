1925988cf7b48259e24c210aff07744352362ad1
Marc A. Donis <mad7@runbox.com>
2018-07-07 21:47:57 +0200

    .

--- main.js
+++ main.js
@@ -3,27 +3,25 @@ var baseRadius = 60;
 
 function onLoad() {
 	canvas = $("#canvas")[0];
-	canvas.width = window.innerWidth - 20;
-	canvas.height = window.innerHeight - 20;
 	context = canvas.getContext("2d");
-	maxX = canvas.width / baseRadius;
-	maxY = canvas.height / (baseRadius * Math.sqrt(3));
 	setInterval(step, 20);
-
 }
 
 function step() {
+	canvas.width = window.innerWidth - 20;
+	canvas.height = window.innerHeight - 20;
+	maxX = canvas.width / baseRadius;
+	maxY = canvas.height / (baseRadius * Math.sqrt(3));
+
 	context.clearRect(0, 0, canvas.width, canvas.height);
 
-	var isOddRow = false;
 	var height = canvas.getClientRects()[0].height;
 	var width = canvas.getClientRects()[0].width;
 	for (var y = 0; y < maxY; y++) {
-		isOddRow = !isOddRow;
-		for (var x = isOddRow ? 0 : 0.5; x < maxX; x++) {
+		for (var x = y % 2 ? 0 : 0.5; x < maxX; x++) {
 			var r = 255 * Math.sin(0.5 * Math.PI * y / maxY);
-			var g = 128 * (1 + Math.sin((isOddRow ? 1 : -1) * t / 27));
-			var b = 128 * (1 + Math.sin((isOddRow ? 1 : -1) * t / 17));
+			var g = 127 * (1 + Math.sin((y % 2 ? 1 : -1) * t / 27));
+			var b = 127 * (1 + Math.sin((y % 2 ? 1 : -1) * t / 17));
 			var color = "rgba(" + [ r, g, b ].join(",") + ", 0.7)";
 			var r = baseRadius * ((1 + Math.sin(Math.PI * ((t / 8) + x + y) / (maxX + maxY))) / 2);
 			drawCircle({
@@ -39,8 +37,8 @@ function step() {
 
 function drawCircle(p, r, color) {
 	var grd = context.createRadialGradient(p.x, p.y, 0, p.x, p.y, r);
-	grd.addColorStop(0, color);
-	grd.addColorStop(1, "rgba(0, 0, 0, 0.2)");
+	grd.addColorStop(0.25, color);
+	grd.addColorStop(1, "rgba(0, 0, 0, 0.1)");
 
 	// Fill with gradient
 	context.beginPath();
